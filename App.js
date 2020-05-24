a6931146ecd7d876887bc6025dceaddb07b66349
joaopfsilva <j0a0.silva.kle@gmail.com>
2020-05-24 12:16:34 +0200

    react-native: text, textInput, Hook, Button, Image

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 export default function App() {
+  const [outputText, setOuputText] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a Text</Text>
+      <TextInput
+        style={{height: 100}}
+        placeholder='This is a Text Input'
+        onChangeText={outputText => setOuputText(outputText)}
+        defaultValue={outputText}
+      />
+      <Button title='This is a button' onPress={() => setOuputText('You pressed the button')}/>
+      <Image
+      source={{uri: 'https://images.pexels.com/photos/1680140/pexels-photo-1680140.jpeg'}}
+      style={{width: 200, height: 200, margin: 40}}
+      />
     </View>
   );
 }
